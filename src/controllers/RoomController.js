57a14680c9a8fa9656f92bbf18364d8c4b8d439c
Alana Ragusa <alana.ragusarabello@gmail.com>
2021-10-06 17:37:38 -0300

    Add files via upload

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -5,19 +5,30 @@ module.exports = {
         const db = await Database()
         const pass = req.body.password
         let roomId
+        let isRoom = true
+        while(isRoom){
+            /* gera o número da sala */
+            for(var i = 0; i < 6; i++){
+                i == 0 ? roomId = Math.floor(Math.random() * 10).toString() : 
+                roomId += Math.floor(Math.random() * 10).toString()
+            }
 
-        for(var i = 0; i < 6; i++){
-            i == 0 ? roomId = Math.floor(Math.random() * 10).toString() : 
-            roomId += Math.floor(Math.random() * 10).toString()
+            /* verificar se o número da sala já existe */
+            const roomsExistIds = await db.all(`SELECT id FROM rooms`)
+            isRoom = roomsExistIds.some(roomExistId => roomsExistIds === roomId)
+
+            if(!isRoom){
+                /* insere a sala no banco de dados */ 
+                await db.run(`INSERT INTO rooms (
+                    id,
+                    pass
+                ) VALUES (
+                    ${parseInt(roomId)},
+                    ${pass}
+                )`)
+            }
         }
         
-        await db.run(`INSERT INTO rooms (
-            id,
-            pass
-        ) VALUES(
-           ${parseInt(roomId)},
-           ${pass}
-        )`)
 
         await db.close()
 
